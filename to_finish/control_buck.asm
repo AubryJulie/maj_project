04b3c7d621f89b20be86d2f64638585a0cf84a58
Aubry Julie <43893665+AubryJulie@users.noreply.github.com>
2019-04-01 22:49:05 +0200

    Add files via upload

--- to_finish/control_buck.asm
+++ to_finish/control_buck.asm
@@ -11,8 +11,9 @@
 ; -> on va utiliser des shifts!!!
 ;KP	= 0.04 -> 1/32 = 0.031
 ;Ki = 0.005 -> 1/128 = 0.0078
-REFH EQU	0x09 ;0x091B = 2.33 V
-REFL EQU	0x1B ;
+REFH EQU	0x0D ;0x0DA7 = 3.5 V
+REFL EQU	0xA7 ;
+MAXDUTY EQU 0x41 ;
 
 ; That is where the MCU will start executing the program (0x00)
 	org 	0x00
@@ -45,6 +46,7 @@ initialisation:
 	sumerrorl
 	total2h
 	total2l
+	config_shift	; bit7: use in shift operation.
 	endc
 	
 	;set all the variables to 0
@@ -59,8 +61,16 @@ initialisation:
 	movwf	sumerrorl
 	movwf	total2h
 	movwf	total2l
+	movwf	config_shift
 	
 	; Configure Pin
+	
+	; Configure Port B
+	movlb   0x0F
+	clrf    TRISB               ; All pins of PORTB are output
+	movlb   0x0F
+	movlw   b'00000000'
+	movwf   LATB                ; RB0..7 = 0 (pin 21)
 	
 	; Configure AN0 (Pin 2)
 	bsf		ANCON0,0	; configured as an analog channel
@@ -83,12 +93,19 @@ initialisation:
 	movwf	ADCON2		; A/D Acquisition Time = 2 TAD | A/D Conversion Clock = FOSC/8 = 1Mhz (minimal for 8Mhz clock) 
 	bsf		ADCON0,0	; ADC on
 	
+	; Configuration Timer4 for use with PWM
+	movlb	0x0F
+	movlw	0x41
+	movwf	PR4		    ; PR4 = 0x41 = 65
+    movlw	b'00000000'
+	movwf	T4CON
+	
 	; Setup PWM
 	movlb 0x0F
-	movlw b'00011100'	; 000011xx (PWM mode, no prescale and no postscale)
+	movlw b'00111100'	; 000011xx (PWM mode, no prescale and no postscale)
 	movwf CCP5CON
 
-	movlw b'00101100'		; MSB of duty cycle 
+	movlw b'01001111'		; MSB of duty cycle 30%
 	movwf CCPR5L
 	bsf CCPTMRS,4  ; CCP5 en PWM
 
@@ -118,59 +135,27 @@ high_interrupt_routine:
 	bsf	ADCON0,1	; ADC go
 
 end_if_timer4:
+	movlb	0x0F
 	btfss	PIR1, 6		; Test the AD interrupt flag
 	goto	end_if_ADC
 	
 	bcf		PIR1, 6
 
 pi:
-	movlb 	0x01
 	movff 	ADRESH, adch
 	movff 	ADRESL, adcl
 	
-	; ref - adc => error
+	; adc - ref => error
 	MOVLW	REFL
 	MOVWF	errorl
-	MOVF	adcl, W
-	SUBWF	errorl, F
+	MOVF	errorl, W
+	SUBWF	adcl, W
+	MOVWF	errorl
 	MOVLW	REFH
 	MOVWF	errorh
-	MOVF	adch, W
-	SUBWFB	errorh, F
-	
-	;total = error*kp = error/32 -> shift of 5 bits
-	MOVFF 	errorl, totall
-	MOVFF 	errorh, totalh
-	; /2
-	bcf 	STATUS, C
-	btfsc   totalh, 7
-	bsf		STATUS, C
-	rrcf 	totalh
-	rrcf 	totall
-	; /4
-	bcf 	STATUS, C
-	btfsc   totalh, 7
-	bsf		STATUS, C
-	rrcf 	totalh
-	rrcf 	totall
-	; /8
-	bcf 	STATUS, C
-	btfsc   totalh, 7
-	bsf		STATUS, C
-	rrcf 	totalh
-	rrcf 	totall
-	; /16
-	bcf 	STATUS, C
-	btfsc   totalh, 7
-	bsf		STATUS, C
-	rrcf 	totalh
-	rrcf 	totall
-	; /32
-	bcf 	STATUS, C
-	btfsc   totalh, 7
-	bsf		STATUS, C
-	rrcf 	totalh
-	rrcf 	totall
+	MOVF	errorh, W
+	SUBWFB	adch, W
+	MOVWF	errorh
 	
 	; sumerror + error => sumerror
 	MOVF 	errorl, W
@@ -178,9 +163,31 @@ pi:
 	MOVF 	errorh, W
 	ADDWFC 	sumerrorh, F
 	
-	;total2 = sumerror*ki = sumerror/128 -> shift of 7 bits
+end_sumerror_ki:
+	
+	;total2 = sumerror/4 -> shift of 2 bits
 	MOVFF 	sumerrorl, total2l
 	MOVFF 	sumerrorh, total2h
+	; /2
+	bcf 	STATUS, C
+	btfsc   totalh, 7
+	bsf		STATUS, C
+	rrcf 	total2h
+	rrcf 	total2l
+	; /4
+	bcf 	STATUS, C
+	btfsc   totalh, 7
+	bsf		STATUS, C
+	rrcf 	total2h
+	rrcf 	total2l
+	
+	; error + sumerror/4
+	MOVF 	errorl, W
+	ADDWF 	total2l, F
+	MOVF 	errorh, W
+	ADDWFC 	total2h, F
+	
+	;total = kp*error+ki*sumerror -> shift of 5 bits
 	; /2
 	bcf 	STATUS, C
 	btfsc   total2h, 7
@@ -211,24 +218,33 @@ pi:
 	bsf		STATUS, C
 	rrcf 	total2h
 	rrcf 	total2l
-	; /64
-	bcf 	STATUS, C
-	btfsc   total2h, 7
-	bsf		STATUS, C
-	rrcf 	total2h
-	rrcf 	total2l
-	; /128
-	bcf 	STATUS, C
-	btfsc   total2h, 7
-	bsf		STATUS, C
-	rrcf 	total2h
-	rrcf 	total2l
+
+	movf	total2h
+	btfss	STATUS, N	; Skip if Set
+	goto	end_total
+	movlw	0x00		; MSB of duty cycle 0%
+	movwf	CCPR5L
+	movlw   0x04
+	xorwf   LATB, 1	; RB2 = !RB2
+	goto	end_pi
 	
-	;total = total + total2
-	MOVF 	total2l, W
-	ADDWF 	totall, F
-	MOVF 	total2h, W
-	ADDWFC 	totalh, F
+end_total:
+	btfss	STATUS, Z	; Skip if Set
+	goto	end_total_max
+	movf	total2l, W
+	movwf	CCPR5L		; duty = totall
+	goto	end_pi
+	
+end_total_max
+	movlw	0x41	; MSB of duty cycle %
+	movwf	CCPR5L
+	movlw   0x04
+	xorwf   LATB, 1	; RB2 = !RB2
+	
+end_pi:
+	movlw   0x01
+	xorwf   LATB, 1	; RB0 = !RB0
+	
 	
 end_if_ADC:
 		retfie
