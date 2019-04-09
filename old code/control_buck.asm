5e170949c3aa51b9d10312fb79ceeec619044cad
Aubry Julie <43893665+AubryJulie@users.noreply.github.com>
2019-04-09 10:42:32 +0200

    Add files via upload

--- old code/control_buck.asm
+++ old code/control_buck.asm
@@ -5,15 +5,20 @@
 ;                                                             ;
 ; *********************************************************** ;
 
-	processor	18F25K80
-	#include	"config18.inc"
+processor	18F25K80
+#include	"config18.inc"
 
 ; -> on va utiliser des shifts!!!
 ;KP	= 0.04 -> 1/32 = 0.031
 ;Ki = 0.005 -> 1/128 = 0.0078
+DUTYMAXH EQU 0x01
+DUTYMAXL EQU 0x08
+DUTYREFH EQU 0x00
+DUTYREFL EQU 0x4D
 REFH EQU	0x0D ;0x0DA7 = 3.5 V
 REFL EQU	0xA7 ;
-MAXDUTY EQU 0x41 ;
+DUTYFLAGMIN EQU 0
+DUTYFLAGMAX EQU 1
 
 ; That is where the MCU will start executing the program (0x00)
 	org 	0x00
@@ -40,13 +45,20 @@ initialisation:
 	adcl
 	errorh
 	errorl
-	totalh
-	totall
-	sumerrorh
-	sumerrorl
-	total2h
-	total2l
-	config_shift	; bit7: use in shift operation.
+	sumerror1
+	sumerror2
+	sumerror3
+	sumerror4
+	total1
+	total2
+	total3
+	total4
+	dutyflags
+	dutyl
+	tmp1
+	tmp2
+	tmp3
+	tmp4
 	endc
 	
 	;set all the variables to 0
@@ -55,13 +67,20 @@ initialisation:
 	movwf	adcl
 	movwf	errorh
 	movwf	errorl
-	movwf	totalh
-	movwf	totall
-	movwf	sumerrorh
-	movwf	sumerrorl
-	movwf	total2h
-	movwf	total2l
-	movwf	config_shift
+	movwf	sumerror1
+	movwf	sumerror2
+	movwf	sumerror3
+	movwf	sumerror4
+	movwf	total1
+	movwf	total2
+	movwf	total3
+	movwf	total4
+	movwf	dutyflags
+	movwf	dutyl
+	movwf	tmp1
+	movwf	tmp2
+	movwf	tmp3
+	movwf	tmp4
 	
 	; Configure Pin
 	
@@ -73,7 +92,7 @@ initialisation:
 	movwf   LATB                ; RB0..7 = 0 (pin 21)
 	
 	; Configure AN0 (Pin 2)
-	bsf		ANCON0,0	; configured as an analog channel
+	bsf	ANCON0,0	; configured as an analog channel
 
     ; Configuration of clock - 8MHz - 
 	movlb	0x0F
@@ -81,7 +100,7 @@ initialisation:
 	movwf	OSCCON		    ; configure oscillator (cf datasheet SFR OSCCON)
 	
 	; Configure ADC
-	; 2,33V nominal value with vref+=4.1V -> 0x91B
+	; 3.5V nominal value with vref+=4.1V -> 0x0DA7
 	; 1 conversion = 15 TAD = 15µs
 	; conversion f=30khz -> every 33µs
 	movlb	0x0F
@@ -91,13 +110,13 @@ initialisation:
 	movwf	ADCON1		; Selects the special trigger from the ECCP1 | Internal VREF+ (4.1V) | Analog Negative Channel Select bits  Channel 00 (AVSS)
 	movlw	b'10001001'
 	movwf	ADCON2		; A/D Acquisition Time = 2 TAD | A/D Conversion Clock = FOSC/8 = 1Mhz (minimal for 8Mhz clock) 
-	bsf		ADCON0,0	; ADC on
+	bsf	ADCON0,0	; ADC on
 	
 	; Configuration Timer4 for use with PWM
 	movlb	0x0F
 	movlw	0x41
 	movwf	PR4		    ; PR4 = 0x41 = 65
-    movlw	b'00000000'
+	movlw	b'00000000'
 	movwf	T4CON
 	
 	; Setup PWM
@@ -131,7 +150,6 @@ high_interrupt_routine:
 	goto	end_if_timer4
 	
 	bcf	PIR4, 7
-	
 	bsf	ADCON0,1	; ADC go
 
 end_if_timer4:
@@ -139,475 +157,262 @@ end_if_timer4:
 	btfss	PIR1, 6		; Test the AD interrupt flag
 	goto	end_if_ADC
 	
-	bcf		PIR1, 6
+	bcf	PIR1, 6
 
 pi:
-	movff 	ADRESH, adch
-	movff 	ADRESL, adcl
+	
+	movf	ADRESH, W
+	movlb	0x01
+	movwf	adch
+	movlb	0x0F
+	movf	ADRESL, W
+	movlb	0x01
+	movwf	adcl
 	
 	; ref - adc => error
-	movlb 	0x01
+	movlb	0x01
 	MOVLW	REFL
 	MOVWF	errorl
 	MOVF	adcl, W
-	SUBWF	errorl, F	; REFL-adcl
-	movlb 	0x0F
-	btfsc	STATUS, N	; skip if clear
-	goto	error_nega_l		
-	movlb 	0x01
-	MOVLW	REFH		; adcl positive
-	MOVWF	errorh
-	MOVF	adch, W
-	SUBWFB	errorh, F	; REFH-adch
-	movlb 	0x0F
-	btfss	STATUS, N	; skip if set
-	goto	end_error_kp		
-	; adch negative/adcl positive
-	movlb 	0x01
-	INCF	errorh, F	; errorh+1
-	MOVLW	0x80
-	ADDWF	errorl, F	;80+errorl
-	goto	end_error_kp
-	
-error_nega_l:; adcl negative
-	movlb 	0x01
+	SUBWF	errorl, F
 	MOVLW	REFH
 	MOVWF	errorh
 	MOVF	adch, W
-	SUBWFB	errorh, F	; REFH-adch
-	movlb 	0x0F
-	btfsc	STATUS, N	; skip if clear
-	goto	end_error_kp
-	; adch positive/adcl negative
-	movlb 	0x0F
-	btfsc	STATUS, Z	; skip if clear
-	goto	end_error_kp; errorh = 0
-	movlb 	0x01
-	DECF	errorh, F	; errorh-1
-	MOVLW	0x7F
-	ADDWF	errorl, F	
-	incf	errorl, F	; 7F+errorl+1
-	goto	end_error_kp
+	SUBWFB	errorh, F
 	
-end_error_kp:
-	movlb 	0x01
-	;total = error*kp = error/32 -> shift of 5 bits
-	MOVFF 	errorl, totall	; totall=errorl
-	MOVFF 	errorh, totalh	; totalh=errorh
-	; /2
-	movlb 	0x0F
-	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   totalh, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totalh is negative
-	movlb 	0x01
-	rrcf 	totalh
-	bcf		config_shift, 7
-	movlb 	0x0F
-	btfsc	STATUS, C	; skip if clear
-	movlb 	0x01
-	bsf		config_shift, 7
-	movlb 	0x0F
-	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   totall, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totall is negative
-	movlb 	0x01
-	rrcf 	totall
-	bcf		totall, 6
-	btfsc	config_shift, 7	; skip if clear
-	bsf		totall, 6
-	; /4
-	movlb 	0x0F
-	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   totalh, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totalh is negative
-	movlb 	0x01
-	rrcf 	totalh
-	bcf		config_shift, 7
-	movlb 	0x0F
-	btfsc	STATUS, C	; skip if clear
-	movlb 	0x01
-	bsf		config_shift, 7
-	movlb 	0x0F
-	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   totall, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totall is negative
-	movlb 	0x01
-	rrcf 	totall
-	bcf		totall, 6
-	btfsc	config_shift, 7	; skip if clear
-	bsf		totall, 6
-	; /8
-	movlb 	0x0F
-	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   totalh, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totalh is negative
-	movlb 	0x01
-	rrcf 	totalh
-	bcf		config_shift, 7
-	movlb 	0x0F
-	btfsc	STATUS, C	; skip if clear
-	movlb 	0x01
-	bsf		config_shift, 7
-	movlb 	0x0F
-	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   totall, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totall is negative
-	movlb 	0x01
-	rrcf 	totall
-	bcf		totall, 6
-	btfsc	config_shift, 7	; skip if clear
-	bsf		totall, 6
-	; /16
-	movlb 	0x0F
-	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   totalh, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totalh is negative
-	movlb 	0x01
-	rrcf 	totalh
-	bcf		config_shift, 7
-	movlb 	0x0F
-	btfsc	STATUS, C	; skip if clear
-	movlb 	0x01
-	bsf		config_shift, 7
-	movlb 	0x0F
-	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   totall, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totall is negative
-	movlb 	0x01
-	rrcf 	totall
-	bcf		totall, 6
-	btfsc	config_shift, 7	; skip if clear
-	bsf		totall, 6
-	; /32
-	movlb 	0x0F
-	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   totalh, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totalh is negative
-	movlb 	0x01
-	rrcf 	totalh
-	bcf		config_shift, 7
-	movlb 	0x0F
-	btfsc	STATUS, C	; skip if clear
-	movlb 	0x01
-	bsf		config_shift, 7
-	movlb 	0x0F
-	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   totall, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totall is negative
-	movlb 	0x01
-	rrcf 	totall
-	bcf		totall, 6
-	btfsc	config_shift, 7	; skip if clear
-	bsf		totall, 6
+;	btfss	errorh,7
+;	goto	ledBlink
+;	movlb	0x0F
+;	bcf LATB, 2
+;	goto endLedBlink
+;ledBlink:
+;	movlb	0x0F
+;	bsf LATB, 2
+;	
+;endLedBlink:
+;   movlb 0x01
 	
+	; Anti windup
+	BTFSC   errorh, 7 ;skip if clear
+	GOTO    antiwindupneg
+	; Positive error, check if duty == max
+	BTFSC   dutyflags, DUTYFLAGMAX
+	GOTO    end_sumerror_ki
+	GOTO    sum_error
+antiwindupneg:
+	; Negative error, check if duty == min
+	BTFSC   dutyflags, DUTYFLAGMIN
+	GOTO    end_sumerror_ki
+
+sum_error:
 	; sumerror + error => sumerror
-	movlb 	0x01
 	MOVF 	errorl, W
-	ADDWF 	sumerrorl, F
-	movlb 	0x0F
-	btfsc	STATUS, N	; skip if clear
-	movlb 	0x01
-	goto	sumerror_nega_l	
-	MOVF 	errorh, W	;sumerrorl positive
-	ADDWF 	sumerrorh, F
-	movlb 	0x0F
-	btfss	STATUS, N	; skip if set
-	goto	end_sumerror_ki
-	; sumerrorl positive/sumerrorh negative
-	movlb 	0x01
-	INCF	sumerrorh, F	; sumerrorh+1
-	MOVLW	0x80
-	ADDWF	sumerrorl, F	;80+sumerrorl
-	goto	end_sumerror_ki
-	
-sumerror_nega_l:;sumerrorl negative
-	movlb 	0x01
+	ADDWF 	sumerror1, F
 	MOVF 	errorh, W
-	ADDWF 	sumerrorh, F
-	movlb 	0x0F
-	btfsc	STATUS, N	; skip if clear
-	movlb 	0x01
-	goto	end_sumerror_ki
-	; sumerrorh positive/sumerrorl negative
-	movlb 	0x0F
-	btfsc	STATUS, Z	; skip if clear
-	movlb 	0x01
-	goto	end_sumerror_ki; sumerrorh = 0
-	DECF	sumerrorh, F	; sumerrorh-1
-	MOVLW	0x7F
-	ADDWF	sumerrorl, F	
-	incf	sumerrorl, F	; 7F+sumerrorl+1
-	goto	end_sumerror_ki
+	ADDWFC 	sumerror2, F
+	movlw	0x0
+	BTFSC   errorh, 7
+	movlw   0xFF
+	ADDWFC 	sumerror3, F
+	ADDWFC 	sumerror4, F
+	
+	btfss	sumerror4, 7
+	goto	ledBlink
+	movlb	0x0F
+	bsf LATB, 1
+	goto endLedBlink
+ledBlink:
+	movlb	0x0F
+	bcf LATB, 1
+	
+endLedBlink:
+	movlb 0x01
 	
 end_sumerror_ki:
 	
-	;total2 = sumerror*ki = sumerror/128 -> shift of 7 bits
-	movlb 	0x01
-	MOVFF 	sumerrorl, total2l
-	MOVFF 	sumerrorh, total2h
+	;total2 = sumerror/4 -> shift of 2 bits
+	movf	sumerror1, W
+	movwf	total1
+	movf	sumerror2, W
+	movwf	total2
+	movf	sumerror3, W
+	movwf	total3
+	movf	sumerror4, W
+	movwf	total4
 	; /2
-	movlb 	0x0F
 	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   total2h, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totalh is negative
-	movlb 	0x01
-	rrcf 	total2h
-	bcf		config_shift, 7
-	movlb 	0x0F
-	btfsc	STATUS, C	; skip if clear
-	movlb 	0x01
-	bsf		config_shift, 7
-	movlb 	0x0F
-	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   total2l, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totall is negative
-	movlb 	0x01
-	rrcf 	total2l
-	bcf		total2l, 6
-	btfsc	config_shift, 7	; skip if clear
-	bsf		total2l, 6
+	btfsc   total4, 7
+	bsf	STATUS, C
+	rrcf 	total4
+	rrcf 	total3
+	rrcf 	total2
+	rrcf 	total1
 	; /4
-	movlb 	0x0F
 	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   total2h, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totalh is negative
-	movlb 	0x01
-	rrcf 	total2h
-	bcf		config_shift, 7
-	movlb 	0x0F
-	btfsc	STATUS, C	; skip if clear
-	movlb 	0x01
-	bsf		config_shift, 7
-	movlb 	0x0F
-	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   total2l, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totall is negative
-	movlb 	0x01
-	rrcf 	total2l
-	bcf		total2l, 6
-	btfsc	config_shift, 7	; skip if clear
-	bsf		total2l, 6
-	; /8
-	movlb 	0x0F
-	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   total2h, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totalh is negative
-	movlb 	0x01
-	rrcf 	total2h
-	bcf		config_shift, 7
-	movlb 	0x0F
-	btfsc	STATUS, C	; skip if clear
-	movlb 	0x01
-	bsf		config_shift, 7
-	movlb 	0x0F
-	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   total2l, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totall is negative
-	movlb 	0x01
-	rrcf 	total2l
-	bcf		total2l, 6
-	btfsc	config_shift, 7	; skip if clear
-	bsf		total2l, 6
-	; /16
-	movlb 	0x0F
-	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   total2h, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totalh is negative
-	movlb 	0x01
-	rrcf 	total2h
-	bcf		config_shift, 7
-	movlb 	0x0F
-	btfsc	STATUS, C	; skip if clear
-	movlb 	0x01
-	bsf		config_shift, 7
-	movlb 	0x0F
-	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   total2l, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totall is negative
-	movlb 	0x01
-	rrcf 	total2l
-	bcf		total2l, 6
-	btfsc	config_shift, 7	; skip if clear
-	bsf		total2l, 6
-	; /32
-	movlb 	0x0F
-	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   total2h, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totalh is negative
-	movlb 	0x01
-	rrcf 	total2h
-	bcf		config_shift, 7
-	movlb 	0x0F
-	btfsc	STATUS, C	; skip if clear
-	movlb 	0x01
-	bsf		config_shift, 7
-	movlb 	0x0F
-	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   total2l, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totall is negative
-	movlb 	0x01
-	rrcf 	total2l
-	bcf		total2l, 6
-	btfsc	config_shift, 7	; skip if clear
-	bsf		total2l, 6
-	; /64
-	movlb 	0x0F
-	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   total2h, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totalh is negative
-	movlb 	0x01
-	rrcf 	total2h
-	bcf		config_shift, 7
-	movlb 	0x0F
-	btfsc	STATUS, C	; skip if clear
-	movlb 	0x01
-	bsf		config_shift, 7
-	movlb 	0x0F
-	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   total2l, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totall is negative
-	movlb 	0x01
-	rrcf 	total2l
-	bcf		total2l, 6
-	btfsc	config_shift, 7	; skip if clear
-	bsf		total2l, 6
-	; /128
-	movlb 	0x0F
-	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   total2h, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totalh is negative
-	movlb 	0x01
-	rrcf 	total2h
-	bcf		config_shift, 7
-	movlb 	0x0F
-	btfsc	STATUS, C	; skip if clear
-	movlb 	0x01
-	bsf		config_shift, 7
-	movlb 	0x0F
-	bcf 	STATUS, C
-	movlb 	0x01
-	btfsc   total2l, 7	; skip if clear
-	movlb 	0x0F
-	bsf		STATUS, C	; totall is negative
-	movlb 	0x01
-	rrcf 	total2l
-	bcf		total2l, 6
-	btfsc	config_shift, 7	; skip if clear
-	bsf		total2l, 6
+	btfsc   total4, 7
+	bsf	STATUS, C
+	rrcf 	total4
+	rrcf 	total3
+	rrcf 	total2
+	rrcf 	total1
 	
-	; total = total + total2
-	movlb 	0x01
-	MOVF 	total2l, W
-	ADDWF 	totall, F
-	movlb 	0x0F
-	btfsc	STATUS, N	; skip if clear
-	movlb 	0x01
-	goto	total_nega_l	
-	MOVF 	total2h, W	;totall positive
-	ADDWF 	totalh, F
-	movlb 	0x0F
-	btfss	STATUS, N	; skip if set
-	goto	end_total
-	; totall positive/totalh negative
-	INCF	totalh, F	; totalh+1
-	MOVLW	0x80
-	ADDWF	totall, F	; 80+totall
-	goto	end_total_nega
+	; movlw	0
+	; movwf	totall
+	; movwf	totalh
 	
-sumerror_nega_l:; totall negative
+	; error + sumerror/4
+	MOVF 	errorl, W
+	ADDWF 	total1, F
 	MOVF 	errorh, W
-	ADDWF 	sumerrorh, F
-	movlb 	0x0F
-	btfsc	STATUS, N	; skip if clear
-	movlb 	0x01
-	goto	end_total_nega
-	; totalh positive/totall negative
-	movlb 	0x0F
-	btfsc	STATUS, Z	; skip if clear
-	movlb 	0x01
-	goto	end_total_nega	; totalh = 0
-	DECF	totalh, F	; totalh-1
-	MOVLW	0x7F
-	ADDWF	totall, F	
-	incf	totall, F	; 7F+totall+1
-	goto	end_total
+	ADDWFC 	total2, F
+	movlw	0x0
+	BTFSC   errorh, 7
+	movlw   0xFF
+	ADDWFC 	total3, F
+	ADDWFC 	total4, F
+	
+	
+	;total = kp*error+ki*sumerror -> shift of 5 bits
+	; /2
+	bcf 	STATUS, C
+	btfsc   total4, 7
+	bsf	STATUS, C
+	rrcf 	total4
+	rrcf 	total3
+	rrcf 	total2
+	rrcf 	total1
+	; /4
+	bcf 	STATUS, C
+	btfsc   total4, 7
+	bsf	STATUS, C
+	rrcf 	total4
+	rrcf 	total3
+	rrcf 	total2
+	rrcf 	total1
+	; /8
+	bcf 	STATUS, C
+	btfsc   total4, 7
+	bsf	STATUS, C
+	rrcf 	total4
+	rrcf 	total3
+	rrcf 	total2
+	rrcf 	total1
+	; /16
+	bcf 	STATUS, C
+	btfsc   total4, 7
+	bsf	STATUS, C
+	rrcf 	total4
+	rrcf 	total3
+	rrcf 	total2
+	rrcf 	total1
+	; /32
+	bcf 	STATUS, C
+	btfsc   total4, 7
+	bsf	STATUS, C
+	rrcf 	total4
+	rrcf 	total3
+	rrcf 	total2
+	rrcf 	total1
 
-end_total_nega:
-	movlb 	0x0F
-	movlw	0x00			; MSB of duty cycle 0%
-	movwf	CCPR5L
-	movlw   0x02
-	xorwf   LATB, 1	; RB1 = !RB1
-	goto	end_pi
+	movf	total1, W
+	movwf	tmp1
+	movf	total2, W
+	movwf	tmp2
+	movf	total3, W
+	movwf	tmp3
+	movf	total4, W
+	movwf	tmp4
 	
-end_total:
+	; duty_ref + duty_correction
+;	movlw	DUTYREFL
+;	addwf	tmp0, F
+;	movlw	DUTYREFH
+;	addwfc	tmp1, F
+	
+	; The final duty cycle is in tmp
+	
+	MOVLW 0
+	MOVWF dutyflags
+	; If negative, set to 0
+	BTFSS tmp4, 7 ;skip if set
+	GOTO dutypositive
+	;MOVLW 0 already done above
+	MOVWF  tmp1
+	MOVWF  tmp2
+	BSF dutyflags, DUTYFLAGMIN
+	; To debug
+	movlb	0x0F
+	movlw   0x04
+	xorwf   LATB, 1	; RB2 = !RB2
+	movlb	0x01
+	GOTO dutycontinue
+	
+dutypositive:
+	; Check overflow
+	MOVF tmp4, F
+	BNZ dutyoverflow
+	MOVF tmp3, F
+	BNZ dutyoverflow
+	MOVLW b'11111100' ; Duty cycle only in 10 LSB of tmp (2 LSB of tmp1) since duty high = 0x01.
+	ANDWF tmp2, W ; Check overflow on 5 MSB of tmp1
+	BZ dutycontinue
+
+dutyoverflow:
+	
+	MOVLW  0xFF
+	MOVWF  tmp1
+	MOVWF  tmp2
+	BSF dutyflags, DUTYFLAGMAX
+	; To debug
+	movlb	0x0F
+	movlw   0x04
+	xorwf   LATB, 1	; RB2 = !RB2
+	movlb	0x01
+	
+dutycontinue:
+	; PUT DUTY CYCLE IN SFR
+	; Low significant bits
+	
+	movf	tmp1, W
+	movwf	dutyl
+	movlw 	b'00000011'
+	andwf 	dutyl, F
+	rlncf 	dutyl
+	rlncf 	dutyl
+	rlncf 	dutyl
+	rlncf 	dutyl ; 00110000
+
+	movlb 	0x0F
+	movlw 	b'11001111'
+	andwf 	CCP5CON, F
+
 	movlb 	0x01
-	movf	totall, W
-	movf	totalh, F
+	movf 	dutyl, W
+
 	movlb 	0x0F
-	btfss	STATUS, Z	; skip if set
-	goto	end_total_max
-	movwf	CCPR5L		; duty = totall
-	goto	end_pi
+	IORWF 	CCP5CON, F
 	
-end_total_max
+	movlb 	0x01
+	
+ 	; Most significant bits
+	bcf 	STATUS, C
+	rrcf 	tmp2, F
+	rrcf 	tmp1, F
+	bcf 	STATUS, C
+	rrcf 	tmp2, F
+	rrcf 	tmp1, F
+
+	movf 	tmp1, W	
 	movlb 	0x0F
-	movlw	0x41	; MSB of duty cycle %
-	movwf	CCPR5L
-	movlw   0x02
-	xorwf   LATB, 1	; RB1 = !RB1
+	movwf 	CCPR5L
+	movlb 	0x01
 	
-end_pi:
-	movlb 	0x0F
-	movlw   0x01
-	xorwf   LATB, 1	; RB0 = !RB0
-	
+pid_end:
+	; To debug
+	; movlb	0x0F
+	; movlw   0x02
+	; xorwf   LATB, 1	; RB1 = !RB1
+	; movlb	0x01
 	
 end_if_ADC:
 		retfie
